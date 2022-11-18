148860c04186ea5b8654c18bde8390fb89489a7b
Sean Fong <seancfong@gmail.com>
2022-11-18 11:59:07 -0800

    Improve responsiveness and styling

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,89 @@
-console.log('This will show on the client side');
+console.log('Oh snap, who told you to look at the console?');
+console.log('Nothing useful is going to be here anyways, but feel free to just stay here if you insist.');
 
+// Create chart
+const ctx = document.getElementById('vote-chart').getContext('2d');
+
+const chartData = {
+    labels: [],
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: [],
+            backgroundColor: [],
+        }
+    ]
+};
+
+const chart = new Chart(ctx, {
+    type: 'pie',
+    data: chartData,
+    options: {
+        plugins: {
+            tooltip: false,
+        },
+        responsive: true,
+        
+    }
+});
+
+// Connect to the server
 const socket = io();
+
+
+// Whenever a vote updates the chart
+socket.on('update', (gameData) => {
+    var alphaKeys = Object.entries(gameData.voteOptions);
+    var totalVotes = gameData.totalVotes;
+
+    console.log(totalVotes);
+
+    const data = chart.data;
+
+    // Iterate through alphabet keys
+    let ds = data.datasets[0];
+
+    for (const [key, letterData] of alphaKeys) {
+        let ind = data.labels.indexOf(key);
+
+        if (letterData.votes > 0) {
+            if (data.labels.includes(key)) {
+                // Update label
+                
+                if (letterData.votes > ds.data[ind]) {
+                    ds.data[ind] = letterData.votes;
+                }
+            } else {
+                // Add new label
+                data.labels.push(key);
+                ds.backgroundColor.push(letterData.bgColor);
+                ds.data.push(letterData.votes);
+            }   
+        }   
+    }
+
+    chart.update();
+});
+
+
+const form = document.getElementById('vote-form');
+form.addEventListener('submit', (e) => {
+    // Prevent from reloading
+    e.preventDefault();
+
+    // Get the value
+    const voteInput = document.getElementById('vote-text-input');
+    const userLetter = voteInput.value;
+
+    // Submit valid letter vote
+    if (/^[a-zA-Z]$/.test(voteInput.value)) {
+        voteInput.value = "";
+        console.log(userLetter);
+        socket.emit('vote', userLetter.toUpperCase());
+
+        // lock the input box
+    } else {
+        console.log('Invalid Input');
+    }
+    
+});
--- index.js
+++ index.js
@@ -2,19 +2,111 @@ const path = require('path');
 const express= require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const randomWords = require('random-words');
 
 // Iniitalize backend server
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const DEBUG = true;
+
+var curWords;
+var voteOptions;
+var totalVotes;
+var openVoting;
+
 // Have app serve front-end static files
 app.use(express.static(path.join(__dirname, 'client')));
 
+// Alphabet array 
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+const getRandomColor = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Voting options
+const resetVoteOptions = () => {
+    voteOptions = {};
+    for (var i = 0; i < 26; i++) {
+        let alphaString = `${alphabet[i]}`;
+        voteOptions[alphaString] = {
+            votes: 0,
+            label: alphaString,
+            bgColor: getRandomColor()
+        }
+    }
+};
+
+const checkMajorityTimer = () => {
+    console.log(`Open voting: Checking timer on [${totalVotes}] votes`);
+};
+
 // Will run when client connects to server
 io.on('connection', socket => {
-    console.log('Client connected to server');
+    if (DEBUG) {
+        console.log('Client connected to server');
+    }
+
+    io.emit('update', {
+        voteOptions: voteOptions,
+        totalVotes: totalVotes,
+    });
+
+    socket.on('vote', (letter) => {
+        console.log(letter);
+        if (voteOptions[letter] && openVoting) {
+            voteOptions[letter].votes += 1;
+            totalVotes++;
+            checkMajorityTimer();
+
+            // Update the voteOptions on other devices
+            io.emit('update', {
+                voteOptions: voteOptions,
+                totalVotes: totalVotes,
+            });
+        }
+    });
 });
+
+
+// Game-handling functions
+const generateWords = () => {
+    let genWords = randomWords({exactly:3, wordsPerString:1, formatter: (word) => word.toUpperCase()});
+    for (let word of genWords) {
+        let wordArray = [];
+        for (let letter of word) {
+            wordArray.push(letter);
+        }
+        curWords.push(wordArray);
+    }
+}
+
+const runGameLoop = () => {
+    console.log('New game loop');
+    
+    // reset all votes
+    totalVotes = 0;
+    resetVoteOptions();
+    openVoting = true;
+
+    // generate new words
+    curWords = [];
+    generateWords();
+
+    console.log(voteOptions);
+    console.log(curWords);
+    
+    
+};
+
+runGameLoop();
 
 const PORT = process.env.PORT || 3000;
 
